52fba7e00e5ada7e5a60c46ad4ec88a07117ea44
Younnes Chebli <younnesc@gmail.com>
2022-10-21 12:20:15 +0200

    added database

---
+++ scripts/script.js
@@ -0,0 +1,132 @@
+const collection = [
+    {
+        name: "Vikings",
+        seasons: 6,
+        creation: "Michael Hirst",
+        releaseYear: 2013,
+        picture: "./images/vikings.jpg",
+        genre: [
+            "Historical Drama",
+            "Action",
+            "War"
+        ],
+        cast: [
+            "Travis Fimmel",
+            "Katheryn Winnick",
+            "Gustaf Skarsgård"
+        ]
+    },
+    {
+        name: "Breaking Bad",
+        seasons: 5,
+        creation: "Vince Gilligan",
+        releaseYear: 2008,
+        picture: "./images/breaking-bad.jpg",
+        genre: [
+            "Crime Drama",
+            "Thriller",
+            "Black Comedy"
+        ],
+        cast: [
+            "Bryan Cranston",
+            "Aaron Paul",
+            "Betsy Brandt"
+        ]
+    },
+    {
+        name: "Peaky Blinders",
+        seasons: 6,
+        creation: "Steven Knight",
+        releaseYear: 2013,
+        picture: "./images/peaky-blinders.jpg",
+        genre: [
+            "Historical Fiction",
+            "Crime Drama"
+        ],
+        cast: [
+            "Cillian Murphy",
+            "Paul Anderson",
+            "Helen McCrory"
+        ]
+    },
+    {
+        name: "Suits",
+        seasons: 9,
+        creation: "Aaron Korsh",
+        releaseYear: 2011,
+        picture: "./images/suits.jpg",
+        genre: [
+            "Legal Drama"
+        ],
+        cast: [
+            "Gabriel Macht",
+            "Patrick J. Adams",
+            "Meghan Markle"
+        ]
+    },
+    {
+        name: "Good Doctor",
+        seasons: 6,
+        developer: "David Shore (based on Park Jae-bum)",
+        releaseYear: 2017,
+        picture: "./images/good-doctor.jpg",
+        genre: [
+            "Medical Drama"
+        ],
+        cast: [
+            "Freddie Highmore",
+            "Hill Harper",
+            "Antonia Thomas"
+        ]
+    },
+    {
+        name: "La Casa de Papel",
+        seasons: 5,
+        creation: "	Álex Pina",
+        releaseYear: 2017,
+        picture: "./images/casa-de-papel.jpg",
+        genre: [
+            "Heist",
+            "Crime Drama"
+        ],
+        cast: [
+            "Álvaro Morte",
+            "Úrsula Corberó",
+            "Itziar Ituño"
+        ]
+    },
+    {
+        name: "The Watcher",
+        seasons: 1,
+        creation: "Ryan Murphy and Ian Brennan",
+        releaseYear: 2022,
+        picture: "./images/the-watcher.jpg",
+        genre: [
+            "Mystery Thriller",
+            "Horror"
+        ],
+        cast: [
+            "Bobby Cannavale",
+            "Naomi Watts"
+        ]
+    },
+    {
+        name: "What/If",
+        seasons: 1,
+        creation: "Mike Kelley",
+        releaseYear: 2019,
+        picture: "./images/what-if.jpg",
+        genre: [
+            "Thriller"
+        ],
+        cast: [
+            "Renée Zellweger",
+            "Blake Jenner",
+            "Jane Levy"
+        ]
+    }    
+];
+
+for(const serie of collection) {
+    
+}
